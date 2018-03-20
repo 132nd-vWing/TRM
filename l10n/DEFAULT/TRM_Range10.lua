9d01010a2b7f79db92a6c54e58983b2b38da767f
132nd-Entropy <ber17118@gmail.com>
2018-03-20 12:30:54 +0100

    fix: Range Script not seeing client AC fixed. we should have the 'on the range' menu back now (thanks to  [TAW]Wingthor for the help)

--- l10n/DEFAULT/TRM_Range10.lua
+++ l10n/DEFAULT/TRM_Range10.lua
@@ -1,5 +1,3 @@
-BASE:TraceOnOff(true)
-BASE:TraceClass("RANGE")
 
  -- Strafe pits. Each pit can consist of multiple targets. Here we have two pits and each of the pits has two targets. These are names of the corresponding units defined in the ME.
  local Range10_Strafepit_Table={"Range10_Strafepit1", "Range10_Strafepit2"}
@@ -9,7 +7,14 @@ BASE:TraceClass("RANGE")
 
  -- Create a range object.
  local Range10=RANGE:New("Range 10")
-Range10:DebugON()
+ 
+
+ 
+ 
+ 
+ 
+ 
+--Range10:DebugON()
 Range10:SetMaxStrafeAlt(5000)
  -- Distance between foul line and strafe target. Note that this could also be done manually by simply measuring the distance between the target and the foul line in the ME.
  local Range10_Strafepit=UNIT:FindByName("Range10_Strafepit1")
@@ -28,4 +33,20 @@ Range10:SetMaxStrafeAlt(5000)
 
  -- Start range.
 
- Range10:Start()
+
+
+
+Range10:Start()
+ 
+birthEventandler = EVENTHANDLER:New()
+birthEventandler:HandleEvent(EVENTS.Birth)
+
+function birthEventandler:OnEventBirth(EventData)
+  BASE:F({EventData})
+  -- Get Group Name
+  local birhtGroup = EventData.IniGroup --Wrapper.Group#GROUP
+  local playerName = birhtGroup:GetPlayerName()
+    if playerName ~= nil then -- We have got a player
+   Range10:_AddF10Commands(EventData.IniUnitName)
+  end
+end
