e4fe32f3c1882a10495d718513a008eee0620bb0
132nd-Entropy <EntropySG@gmail.com>
2018-03-25 13:14:43 +0200

    add Ark-UD via ME not via script

--- l10n/DEFAULT/TRM_Config.lua
+++ l10n/DEFAULT/TRM_Config.lua
@@ -25,20 +25,20 @@ SPAWN:New("BLUE AWACS #1"):InitLimit(1,0):InitRepeatOnEngineShutDown():SpawnSche
 SPAWN:New("BLUE AWACS #2"):InitLimit(1,0):InitRepeatOnEngineShutDown():SpawnScheduled(120,0)
 SPAWN:New("RED AGGRESSOR AWACS"):InitLimit(1,0):InitRepeatOnEngineShutDown():SpawnScheduled(120,0)
 
--- Beacons for the Mi-8 -- 
-
-
-SCHEDULER:New(nil,function()
-FARP_Kutaisi = STATIC:FindByName("Windsock_KUTAISI_FARP",true)
-FARP_Kutaisi_Radio = FARP_Kutaisi:GetBeacon()
-FARP_Kutaisi_Radio:RadioBeacon("beaconKUTAISI.ogg",114.166,radio.modulation.AM,150)
-end
-,{},1,60)
-
-
-SCHEDULER:New(nil,function()
-FARP_London = STATIC:FindByName("Windsock_FARP_LONDON",true)
-FARP_London_Radio = FARP_London:GetBeacon()
-FARP_London_Radio:RadioBeacon("beaconLONDON.ogg",114.333,radio.modulation.AM,150)
-end
-,{},31,60)
+---- Beacons for the Mi-8 -- 
+--
+--
+--SCHEDULER:New(nil,function()
+--FARP_Kutaisi = STATIC:FindByName("Windsock_KUTAISI_FARP",true)
+--FARP_Kutaisi_Radio = FARP_Kutaisi:GetBeacon()
+--FARP_Kutaisi_Radio:RadioBeacon("beaconKUTAISI.ogg",114.166,radio.modulation.AM,150)
+--end
+--,{},1,60)
+--
+--
+--SCHEDULER:New(nil,function()
+--FARP_London = STATIC:FindByName("Windsock_FARP_LONDON",true)
+--FARP_London_Radio = FARP_London:GetBeacon()
+--FARP_London_Radio:RadioBeacon("beaconLONDON.ogg",114.333,radio.modulation.AM,150)
+--end
+--,{},31,60)
