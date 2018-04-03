cb25f5567c8df6adbed15b68b7493f7fae624525
132nd-Entropy <ber17118@gmail.com>
2018-04-03 14:28:20 +0200

    fix: added modified MOOSE.lua in which a dispersunderfireoff function was included fix: iterate over each ground group at mission start and set disperseunderfire off

--- l10n/DEFAULT/TRM_Config.lua
+++ l10n/DEFAULT/TRM_Config.lua
@@ -18,16 +18,8 @@ Menu_Range_Options = MENU_MISSION:New("Range Options")
 -- Threat Options -- 
 Menu_Threat_Options = MENU_MISSION:New("Threat Sites")
 
-
-
--- AWACS -- 
-SPAWN:New("BLUE AWACS #1"):InitLimit(1,0):InitRepeatOnEngineShutDown():SpawnScheduled(120,0)
-SPAWN:New("BLUE AWACS #2"):InitLimit(1,0):InitRepeatOnEngineShutDown():SpawnScheduled(120,0)
-SPAWN:New("RED AGGRESSOR AWACS"):InitLimit(1,0):InitRepeatOnEngineShutDown():SpawnScheduled(120,0)
-
----- Beacons for the Mi-8 -- 
---
---
+-- Beacons for the Mi-8 -- 
+-- inactive, currently added via ME
 --SCHEDULER:New(nil,function()
 --FARP_Kutaisi = STATIC:FindByName("Windsock_KUTAISI_FARP",true)
 --FARP_Kutaisi_Radio = FARP_Kutaisi:GetBeacon()
@@ -42,3 +34,41 @@ SPAWN:New("RED AGGRESSOR AWACS"):InitLimit(1,0):InitRepeatOnEngineShutDown():Spa
 --FARP_London_Radio:RadioBeacon("beaconLONDON.ogg",114.333,radio.modulation.AM,150)
 --end
 --,{},31,60)
+
+
+-- AWACS -- 
+SPAWN:New("BLUE AWACS #1"):InitLimit(1,0):InitRepeatOnEngineShutDown():SpawnScheduled(120,0)
+SPAWN:New("BLUE AWACS #2"):InitLimit(1,0):InitRepeatOnEngineShutDown():SpawnScheduled(120,0)
+SPAWN:New("RED AGGRESSOR AWACS"):InitLimit(1,0):InitRepeatOnEngineShutDown():SpawnScheduled(120,0)
+
+
+
+-- DISPERSE UNDER FIRE OFF
+--search for this line in moose: 
+--function CONTROLLABLE:OptionROEOpenFirePossible()
+--
+--then paste the following code at the end of the function
+--
+--function CONTROLLABLE:OptionDisperseOff()
+--  self:F2( { self.ControllableName } )
+--
+--  local DCSControllable = self:GetDCSObject()
+--  if DCSControllable then
+--    local Controller = self:_GetController()
+--
+--      Controller:setOption( 8,0 )
+--
+--    return self
+--  end
+--
+--  return nil
+--end
+--
+
+AllGroundUnits = SET_GROUP:New():FilterCategoryGround()
+AllGroundUnits:FilterStart()
+AllGroundUnits:ForEachGroup(function (allgroups)
+allgroups:OptionDisperseOff() end)
+env.info("We iterated over each Ground Group and set their option to disperse under fire to off")
+-- this will iterate over all existing ground units at mission start and will set the disperse off function that we added to moose
+
