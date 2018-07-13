ac11fcd020aab7faa1814d0f4398aad6c535cd31
132nd-Entropy <EntropySG@gmail.com>
2018-07-13 12:20:00 +0200

    fix: re-wrote AWACS script so they will go RTB instead of AAR when low on fuel

--- l10n/DEFAULT/TRM_Config.lua
+++ l10n/DEFAULT/TRM_Config.lua
@@ -14,21 +14,103 @@ Menu_Threat_Options = MENU_MISSION:New("Threat Sites")
 -- Threat Options --
 
 -- AWACS --
-awacs1 = SPAWN:New("BLUE AWACS #1"):InitLimit(1,0):SpawnScheduled(120,0)
-awacs2 = SPAWN:New("BLUE AWACS #2"):InitLimit(1,0):SpawnScheduled(120,0)
-awacs3 = SPAWN:New("RED AGGRESSOR AWACS"):InitLimit(1,0):SpawnScheduled(20,0)
 
-AWACS  = SET_GROUP:New():FilterPrefixes({"BLUE AWACS", 'IL-78_TANKER_A', "RED AGGRESSOR", 'KC-130_TANKER_B', 'KC-135_SHELL','KC-135_TEXACO','AGGRESSOR_Il78M'}):FilterStart()
+
+
+
+awacs1 = SPAWN:New("BLUE AWACS #1")
+function awacs1spawn()
+  awacs1:OnSpawnGroup(
+    function (awacs1_group)
+      awacs1_unit_fuel_scheduler = SCHEDULER:New(nil,function ()
+        awacs1_unit = awacs1_group:GetUnit(1)
+        if awacs1_unit
+        then
+          awacs1_unit_fuel = awacs1_unit:GetFuel()
+          if awacs1_unit_fuel <= 0.3
+          then
+            awacs1_group:RouteRTB()
+            awacs1_unit_fuel_scheduler:Stop()
+            env.info(awacs1_group:GetName().." is low on fuel and RTBing")
+            awacs1spawn()
+          end
+        else
+          awacs1_unit_fuel_scheduler:Stop()
+          awacs1spawn()
+        end
+      end
+      ,{},5,30)
+    end):Spawn()
+end
+awacs1spawn()
+
+awacs2 = SPAWN:New("BLUE AWACS #2")
+function awacs2spawn()
+  awacs2:OnSpawnGroup(
+    function (awacs2_group)
+      awacs2_unit_fuel_scheduler = SCHEDULER:New(nil,function ()
+        awacs2_unit = awacs2_group:GetUnit(1)
+        if awacs2_unit
+        then
+          awacs2_unit_fuel = awacs2_unit:GetFuel()
+          if awacs2_unit_fuel <= 0.3
+          then
+            awacs2_group:RouteRTB()
+            awacs2_unit_fuel_scheduler:Stop()
+            env.info(awacs2_group:GetName().." is low on fuel and RTBing")
+            awacs2spawn()
+          end
+        else
+          awacs2_unit_fuel_scheduler:Stop()
+          awacs2spawn()
+        end
+      end
+      ,{},5,30)
+    end):Spawn()
+end
+awacs2spawn()
+
+awacs3 = SPAWN:New("RED AGGRESSOR AWACS")
+function awacs3spawn()
+  awacs3:OnSpawnGroup(
+    function (awacs3_group)
+      awacs3_unit_fuel_scheduler = SCHEDULER:New(nil,function ()
+        awacs3_unit = awacs3_group:GetUnit(1)
+        if awacs3_unit
+        then
+          awacs3_unit_fuel = awacs3_unit:GetFuel()
+          if awacs3_unit_fuel <= 0.3
+          then
+            awacs3_group:RouteRTB()
+            awacs3_unit_fuel_scheduler:Stop()
+            env.info(awacs3_group:GetName().." is low on fuel and RTBing")
+            awacs3spawn()
+          end
+        else
+          awacs3_unit_fuel_scheduler:Stop()
+          awacs3spawn()
+        end
+      end
+      ,{},5,30)
+    end):Spawn()
+end
+awacs3spawn()
+
+
+
+
+AWACS_Tankers  = SET_GROUP:New():FilterPrefixes({"BLUE AWACS", 'IL-78_TANKER_A', "RED AGGRESSOR", 'KC-130_TANKER_B', 'KC-135_SHELL','KC-135_TEXACO','AGGRESSOR_Il78M'}):FilterStart()
 SCHEDULER:New(nil,function()
-  AWACS:ForEachGroup(function(_group)
+  AWACS_Tankers:ForEachGroup(function(_group)
     local _unit = _group:GetUnit(1)
     speed = _unit:GetVelocityMPS()
     if speed <= 1 then
-      _group:Destroy()
-      env.info (_group:GetName().." was stuck on the ground and has been despawned")
+    env.info (_unit:GetName().." was stuck on the ground and has been despawned")
+    _group:Destroy()
     end
   end)
-end,{},300,300,0,0)
+end,{},400,330,0,0)
+
 
 -- ARK-UD BEACONS -- 
 --SCHEDULER:New(nil,function()
