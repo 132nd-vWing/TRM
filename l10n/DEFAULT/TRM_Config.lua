f624bff14862e85831aae32791414877692c09aa
132nd-Entropy <ber17118@gmail.com>
2018-10-21 16:31:43 +0200

    chg: Added Range 11 targets (RW range). chg: Adjusted borders for MOA NORTHEAST and MOA NORTHWEST chg: Removed/changed all units that requires MOD's (Part of getting the mission into a more vanilla state in the effort to get the server more stable). chg: Removed veichles from MOD's chg: changed missile trainer intercept distance from 500m to 700m to decrease the chance of kills

--- l10n/DEFAULT/TRM_Config.lua
+++ l10n/DEFAULT/TRM_Config.lua
@@ -5,7 +5,7 @@ _SETTINGS:SetPlayerMenuOff()
 
 
 -- Missile Trainer
-Trainer = MISSILETRAINER:New( 500, "Missile Trainer is active, ONLY if you see this message when entering an Aircraft, Missiles will be intercepted!" )
+Trainer = MISSILETRAINER:New( 700, "Missile Trainer is active, ONLY if you see this message when entering an Aircraft, Missiles will be intercepted!" )
 Trainer:InitMessagesOnOff(true):InitAlertsToAll(true):InitAlertsHitsOnOff(true):InitAlertsLaunchesOnOff(true):InitBearingOnOff(false):InitRangeOnOff(false):InitTrackingOnOff(false):InitTrackingToAll(false):InitMenusOnOff(false)
 -- Missile Trainer
 
