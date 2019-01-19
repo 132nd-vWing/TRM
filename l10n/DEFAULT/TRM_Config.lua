1f886feedad39c7a1292bb56ba68bfc89dc313ba
EntropySG <ber17118@gmail.com>
2019-01-19 13:47:42 +0100

    new: add rescue helicopter to Stennis chg: add soundfiles to Stennis for LSO and Marshal chg: add Rescue Tanker to stennis chg: add helo carrier and Normandy to Stennis chg: update MOOSE

--- l10n/DEFAULT/TRM_Config.lua
+++ l10n/DEFAULT/TRM_Config.lua
@@ -2,14 +2,29 @@
 _SETTINGS:SetPlayerMenuOff()
 -- command center off
 
+stennis_rescuehelo1=RESCUEHELO:New("CVN STENNIS", "Stennis_Rescue")
+stennis_rescuehelo1:SetHomeBase(AIRBASE:FindByName("Perry"))
+stennis_rescuehelo1:SetTakeoffHot()
+stennis_rescuehelo1:Start()
+
+airboss_stennis_tanker= RECOVERYTANKER:New("CVN STENNIS", "TEXACO 2")
+airboss_stennis_tanker:SetTakeoffHot()
+airboss_stennis_tanker:SetRadio(242)
+airboss_stennis_tanker:SetTACAN(10, "TXO")
+airboss_stennis_tanker:SetAltitude(26000)
+airboss_stennis_tanker:SetSpeed(350)
+airboss_stennis_tanker:Start()
+
 airboss_stennis = AIRBOSS:New("CVN STENNIS")
 airboss_stennis:SetICLS(1,'CVN')
 airboss_stennis:SetTACAN(47,X,'CVN')
+airboss_stennis:AddRecoveryWindow("9:01",  "20:59", 1)
+airboss_stennis:AddRecoveryWindow("21:00", "09:00", 3)
+airboss_stennis:SetMarshalRadio(305,FM)
+airboss_stennis:SetLSORadio(264,FM)
+airboss_stennis:SetRecoveryTanker(airboss_stennis_tanker)
+
 airboss_stennis:Start()
-
- 
-
-
 
 -- Missile Trainer
 Trainer = MISSILETRAINER:New(70, "Missile Trainer is active!" )
